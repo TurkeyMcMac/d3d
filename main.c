4fd4860ad4f098b6acceae8df74a0a9a3ca45c93
TurkeyMcMac <jwmhjwmh@gmail.com>
2019-04-29 21:53:10 -0400

    Make the demo 4x4

--- main.c
+++ main.c
@@ -51,18 +51,23 @@ int main(void)
 	d3d_texture *txtr = d3d_new_texture(SIDE, SIDE);
 	d3d_block blk = {{txtr, txtr, txtr, txtr, txtr, txtr}};
 	d3d_camera *cam = d3d_new_camera(2.0, 2.0, COLS, LINES);
-	d3d_board *brd = d3d_new_board(3, 3);
+	d3d_board *brd = d3d_new_board(4, 4);
 	memcpy(txtr->pixels, pixels, SIDE * SIDE);
 	brd->blocks[0] = &blk;
 	brd->blocks[1] = &blk;
 	brd->blocks[2] = &blk;
 	brd->blocks[3] = &blk;
 	brd->blocks[4] = &blk;
-	brd->blocks[5] = &blk;
-	brd->blocks[6] = &blk;
 	brd->blocks[7] = &blk;
 	brd->blocks[8] = &blk;
-	cam->pos.x = 0.4;
+	brd->blocks[9] = &blk;
+	brd->blocks[10] = &blk;
+	brd->blocks[11] = &blk;
+	brd->blocks[12] = &blk;
+	brd->blocks[13] = &blk;
+	brd->blocks[14] = &blk;
+	brd->blocks[15] = &blk;
+	cam->pos.x = 1.4;
 	cam->pos.y = 1.4;
 	init_pairs();
 	for (;;) {
