b21684e19015a654293253310cd0c161fc7af7eb
TurkeyMcMac <jwmhjwmh@gmail.com>
2019-05-03 08:40:00 -0400

    Scale sprite pixel positions by the right amount

--- d3d.c
+++ d3d.c
@@ -268,14 +268,16 @@ void d3d_draw_sprite(d3d_camera *cam, const d3d_sprite *sp)
 	height = atan(sp->scale.y / dist) * 2;
 	start.x = (cam->facing - angle + maxdiff) / 2 / M_PI;
 	start.y = (cam->fov.y - height) / 4 / M_PI;
-	for (size_t x = 0; x < width / cam->fov.x * cam->width; ++x) {
+	width = width / cam->fov.x * cam->width;
+	height = height / cam->fov.y * cam->height;
+	for (size_t x = 0; x < width; ++x) {
 		size_t cx = x + start.x * cam->width;
 		size_t sx = (double)x
-			/ cam->width * sp->txtr->width / sp->scale.x;
-		for (size_t y = 0; y < height / cam->fov.y * cam->height; ++y) {
+			/ width * sp->txtr->width / sp->scale.x;
+		for (size_t y = 0; y < height; ++y) {
 			size_t cy = y + start.y * cam->height;
 			size_t sy = (double)y
-				/ cam->height * sp->txtr->height / sp->scale.y;
+				/ height * sp->txtr->height / sp->scale.y;
 			d3d_pixel p = *GET(sp->txtr, pixels, sx, sy);
 			if (p != sp->transparent) *GET(cam, pixels, cx, cy) = p;
 		}
